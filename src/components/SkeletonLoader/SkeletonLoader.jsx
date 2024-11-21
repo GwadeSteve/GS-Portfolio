21cffae6eda7e516750f5bcc4c69011b9066e583
GSteve12 <gwadesteve12@gmail.com>
2024-11-21 08:16:22 +0100

    Added metadata to the index.html

---
+++ src/components/SkeletonLoader/SkeletonLoader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SkeletonLoader.css'
+
+const SkeletonLoader = () => {
+    return (
+        <div className='skeleton'>
+            
+        </div>
+    )
+}
+
+export default SkeletonLoader
--- src/pages/AboutPage/AboutSection-1/AboutSectionOne.jsx
+++ src/pages/AboutPage/AboutSection-1/AboutSectionOne.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AboutSectionOne.css';
+import SkeletonLoader from '../../../components/SkeletonLoader/SkeletonLoader';
 
 const images = [
     { src: process.env.PUBLIC_URL + '/images/Me4.png', alt: 'Me4' },
@@ -16,9 +17,7 @@ const AboutSectionOne = () => {
                     <p className="xxl-font"><span className='gradient-text'>About</span></p>
                     <div className="Img-collage">
                         {images.map((image, index) => (
-                            <div className="Col-Box" key={index}>
-                                <img src={image.src} alt={image.alt} />
-                            </div>
+                            <ImageWithLoader key={index} src={image.src} alt={image.alt} />
                         ))}
                     </div>
                 </div>
@@ -27,12 +26,28 @@ const AboutSectionOne = () => {
                         From childhood tech enthusiast to aspiring computer vision researcher, my journey in technology continues to fuel my passion and innovation.
                     </p>
                     <p className="s-font">
-                        Hello! I'm Gwade Steve, a passionate Software Developer and aspiring Computer Vision Researcher. My journey into the world of technology began when I was around 8 years old, playing with computers and debugging issues on my parents' cell phones. This early curiosity evolved into a lifelong passion, leading me to pursue a Research Master's in Data Science at the National Higher Polytechnic School of Douala. With a strong foundation in software engineering, I focus on creating innovative solutions that bridge the gap between software development and data science. At NHPSD, I delve deeper into machine learning and computer vision, leveraging various programming languages, frameworks, and technologies to tackle real-world challenges. When I'm not coding or researching, you can find me on the basketball court, throwing some hoops. This balance between tech and sports keeps me energized and inspired to continuously push the boundaries of what's possible with technology.
+                    Hello! I'm Gwade Steve, a passionate Software Developer and aspiring Computer Vision Researcher. My journey into the world of technology began when I was around 8 years old, playing with computers and debugging issues on my parents' cell phones. This early curiosity evolved into a lifelong passion, leading me to pursue a Research Master's in Data Science at the National Higher Polytechnic School of Douala. With a strong foundation in software engineering, I focus on creating innovative solutions that bridge the gap between software development and data science. At NHPSD, I delve deeper into machine learning and computer vision, leveraging various programming languages, frameworks, and technologies to tackle real-world challenges. When I'm not coding or researching, you can find me on the basketball court, throwing some hoops. This balance between tech and sports keeps me energized and inspired to continuously push the boundaries of what's possible with technology.
                     </p>
                 </div>
             </div>
         </div>
     );
 }
+
+const ImageWithLoader = ({ src, alt }) => {
+    const [loading, setLoading] = useState(true);
+
+    return (
+        <div className="Col-Box">
+            {loading && <SkeletonLoader />}
+            <img
+                src={src}
+                alt={alt}
+                onLoad={() => setLoading(false)} 
+                style={{ display: loading ? 'none' : 'block' }} 
+            />
+        </div>
+    );
+};
 
 export default AboutSectionOne;
